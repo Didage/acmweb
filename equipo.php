005334be09542b306c02d84cba89367b6d299c62
Didage <didage@gmail.com>
2016-02-20 17:40:52 -0500

    Prueba para la muestra de imágenes por persona

--- equipo.php
+++ equipo.php
@@ -13,32 +13,48 @@
 		<p>ACM Uniandes es un grupo de estudiantes innovadores, emprendedores y trabajadores. Somos apasionados por la computación, queremos esparcir
 			el interés por ella como ciencia, profesión y arte. Estamos comprometidos con resolver los problemas de nuestra comunidad por medio de la herramienta de transformación más poderosa de todas, la tecnología. Para lograrlo, simplemente necesitamos aglomerar un grupo de gente muy inteligente.</p>
 
-	<section class="box special features">
-				<div class="features-row">
-					<section>
-						<span class="icon major fa-bolt icon1"></span>
-						<h3>Diego García</h3>
-						<p>Amo y señor del conocimiento!</p>
-					</section>
-					<section>
-						<span class="icon major fa-lightbulb-o icon2"></span>
-						<h3>¿Qué hacemos?</h3>
-						<p>Soluciones a problemas que nosotros mismos tenemos como estudiantes y cuya solución se extrapola a más gente. Hacemos software, automatizamos procesos para darles eficiencia. </p>
-					</section>
+	<div class="row">
+					<div class="6u 12u(narrower)">
+						<section class="box special">
+							<span class="image featured image-main"><img src="images/JuntaACM.jpg" alt="" /></span>
+							<h3>Alejandro Tovar</h3>
+							<p>Enterate de quien es que está liderando esta iniciativa y conctáctanos.  </p>
+							<ul class="actions">
+								<li><a href="junta.php" class="button alt">Ver Más</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="6u 12u(narrower)">
+						<section class="box special">
+							<span class="image featured image-main"><img src="images/EquipoACM.jpg" alt="" /></span>
+							<h3>Nuestro Equipo</h3>
+							<p>Somos un equipo interdisciplinario que busca aprender y beneficiar a la comunidad.</p>
+							<ul class="actions">
+								<li><a href="equipo.php" class="button alt">Ver Más</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="6u 12u(narrower)">
+						<section class="box special">
+							<span class="image featured image-main"><img src="https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/s526x395/603830_877112182309501_210003835752543284_n.png?oh=ff4fe4a436baeca9fc8fe6bf100df5ef&oe=55CCF170" alt="" /></span>
+							<h3>Actividades</h3>
+							<p>Hacemos bootcamps, celebramos fechas especiales, hackathons y mucho más. </p>
+							<ul class="actions">
+								<li><a href="actividades.php" class="button alt">Ver Más</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="6u 12u(narrower)">
+						<section class="box special">
+							<span class="image featured image-main"><img src="http://acm.uniandes.edu.co/wordpress/wp-content/uploads/2012/10/hlab2013.jpg" alt="" /></span>
+							<h3>Proyectos</h3>
+							<p>Desarrollamos una variedad de aplicaciones web y móviles, todas con un mismo fin. </p>
+							<ul class="actions">
+								<li><a href="proyectos.php" class="button alt">Ver Más</a></li>
+							</ul>
+						</section>
+					</div>
 				</div>
-				<div class="features-row">
-					<section>
-						<span class="icon major fa-simplybuilt icon3"></span>
-						<h3>¿Cómo lo hacemos?</h3>
-						<p>Haciendo que los innovadores de la universidad colaboren entre si. Experimentamos a altas velocidades, tenemos una toma de decisiones inclusiva y debatimos constructivamente. </p>
-					</section>
-					<section>
-						<span class="icon major fa-question icon4"></span>
-						<h3>¿Qué es ACM?</h3>
-						<p>La Association for Computing Machinery es la organización educativa y científica en computación más grande del mundo. </p>
-					</section>
-				</div>
-			</section>
 
 	</section>
 
