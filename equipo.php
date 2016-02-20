7e017787f4ef1798df8c19d96b8fbfbeeb53e65f
Didage <didage@gmail.com>
2016-02-20 18:53:10 -0500

    Prueba para la muestra de imágenes por persona

--- equipo.php
+++ equipo.php
@@ -12,46 +12,43 @@
 			el interés por ella como ciencia, profesión y arte. Estamos comprometidos con resolver los problemas de nuestra comunidad por medio de la herramienta de transformación más poderosa de todas, la tecnología. Para lograrlo, simplemente necesitamos aglomerar un grupo de gente muy inteligente.</p>
 	</div>
 	<div class="row">
-					<div class="4u 12u(narrower)">
-						<section class="box special">
-							<span class="image featured image-main"><img src="images/JuntaACM.jpg" alt="" /></span>
-							<h3>Alejandro Tovar</h3>
-							<p>Enterate de quien es que está liderando esta iniciativa y conctáctanos.  </p>
-							<ul class="actions">
-								<li><a href="junta.php" class="button alt">Ver Más</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="4u 12u(narrower)">
-						<section class="box special">
-							<span class="image featured image-main"><img src="images/EquipoACM.jpg" alt="" /></span>
-							<h3>Nuestro Equipo</h3>
-							<p>Somos un equipo interdisciplinario que busca aprender y beneficiar a la comunidad.</p>
-							<ul class="actions">
-								<li><a href="equipo.php" class="button alt">Ver Más</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="4u 12u(narrower)">
-						<section class="box special">
-							<span class="image featured image-main"><img src="https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/s526x395/603830_877112182309501_210003835752543284_n.png?oh=ff4fe4a436baeca9fc8fe6bf100df5ef&oe=55CCF170" alt="" /></span>
-							<h3>Actividades</h3>
-							<p>Hacemos bootcamps, celebramos fechas especiales, hackathons y mucho más. </p>
-							<ul class="actions">
-								<li><a href="actividades.php" class="button alt">Ver Más</a></li>
-							</ul>
-						</section>
-					</div>
-					<div class="6u 12u(narrower)">
-						<section class="box special">
-							<span class="image featured image-main"><img src="http://acm.uniandes.edu.co/wordpress/wp-content/uploads/2012/10/hlab2013.jpg" alt="" /></span>
-							<h3>Proyectos</h3>
-							<p>Desarrollamos una variedad de aplicaciones web y móviles, todas con un mismo fin. </p>
-							<ul class="actions">
-								<li><a href="proyectos.php" class="button alt">Ver Más</a></li>
-							</ul>
-						</section>
-					</div>
+		<div class="4u 12u(narrower)">
+			<section class="box special">
+				<span class="image featured image-main"><img src="images/JuntaACM.jpg" alt="" /></span>
+				<h3>Alejandro Tovar</h3>
+				<p>Estudiante en Ingeniería de Sistemas con doble programa en Música
+					<br>Participante en maratones de programación.
+				<br> Tesorero de ACM</p>
+			</section>
+		</div>
+		<div class="4u 12u(narrower)">
+			<section class="box special">
+				<span class="image featured image-main"><img src="images/JuntaACM.jpg" alt="" /></span>
+				<h3>Juan Carlos Saldarriaga</h3>
+				<p>Estudiante en Administración de Empresas con doble programa en Matemáticas
+				<br>Participante en maratones de programación, ajedréz.
+				<br>Director de Bootcamps, organizador del día de Pi, Juez del equipo ganador de Alcodehol</p>
+			</section>
+		</div>
+		<div class="4u 12u(narrower)">
+			<section class="box special">
+				<span class="image featured image-main"><img src="images/JuntaACM.jpg" alt="" /></span>
+				<h3>Ignacio Gomez</h3>
+				<p>Estudiante en Administración de empresas con doble programa en Ingeniería de Sistemas
+				<br>Spotify.com/igomeza
+				<br>Secretario de ACM</p>
+			</section>
+		</div>
+		<div class="4u 12u(narrower)">
+			<section class="box special">
+				<span class="image featured image-main"><img src="images/JuntaACM.jpg" alt="" /></span>
+				<h3>Esteban Dalel</h3>
+				<p>Estudiante en Ingeniería de Sistemas
+				<br>Experto en experiencias swinger, amante de los tríos y de saber de la vida sexual de los demás
+					<br> 3105588735
+				<br>Presidente de ACM, College Manager para Kiwi, estebandalelr.co</p>
+			</section>
+		</div>
 				</div>
 
 	</section>
